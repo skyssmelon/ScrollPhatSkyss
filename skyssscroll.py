8dc9d70653a273a44d2b179ef562ff2f2fd63a99
skyssmelon <47449026+skyssmelon@users.noreply.github.com>
2019-02-08 17:20:34 +0100

    Update skyssscroll.py

--- skyssscroll.py
+++ skyssscroll.py
@@ -1,9 +1,6 @@
 import requests
 import time
 import scrollphathd as sphd
-from scrollphathd.fonts import font3x5
-
-scrollphathd.rotate(180)
 
 r = requests.get('https://skyss.giantleap.no/public/departures?Hours=1&StopIdentifiers=12015491') #henter ruteinfo
 
@@ -12,7 +9,9 @@ d2 = r.json()["PassingTimes"][1]["TripDestination"] #neste avgang representert v
 t1 = r.json()["PassingTimes"][0]["DisplayTime"]
 t2 = r.json()["PassingTimes"][1]["DisplayTime"]
 
-sphd.write_string(d1, ' ', t1, ',  ', d2, ' ', t2, brightness=0.3, font=font3x5)
+str1 = (d1, ' ', t1, ',  ', d2, ' ', t2)
+
+sphd.write_string(str1)
 
 while True:
     sphd.show()
